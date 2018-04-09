dd0a926894094645ca55a00183b887037a2c133a
aayansh <bhartichandankr@gmail.com>
2018-04-09 10:01:25 -0700

    ok

--- src/com/coder/contest/BinaryTree.java
+++ src/com/coder/contest/BinaryTree.java
@@ -6,17 +6,7 @@ import java.util.Vector;
 
 /* A binary tree node has data, pointer to left child
    and a pointer to right child */
-class Node
-{
-    int data;
-    Node left, right;
 
-    public Node(int data)
-    {
-        this.data = data;
-        left = right = null;
-    }
-}
 
 class BinaryTree
 {
---
+++ src/com/coder/contest/Node.java
@@ -0,0 +1,12 @@
+package com.coder.contest;
+
+public class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
