ab0519acb584ee33b5501d164afe48f68d269746
aayansh <bhartichandankr@gmail.com>
2019-06-07 15:18:50 +0530

    ok

---
+++ src/com/coder/contest/JavaStreamExample.java
@@ -0,0 +1,76 @@
+package com.coder.contest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Product{
+    int id;
+    String name;
+    float price;
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+public class JavaStreamExample {
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList<Product>();
+        //Adding Products
+        productsList.add(new Product(1,"HP Laptop",25000f));
+        productsList.add(new Product(2,"Dell Laptop",30000f));
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));
+        productsList.add(new Product(4,"Sony Laptop",28000f));
+        productsList.add(new Product(5,"Apple Laptop",90000f));
+        // max() method to get max Product price
+        // This is more compact approach for filtering data
+        productsList.stream()
+                .filter(product -> product.price == 30000)
+                .forEach(product -> System.out.println(product.name));
+        Product productA = productsList.stream()
+                .max((product1, product2)->
+                        product1.price > product2.price ? 1: -1).get();
+
+        System.out.println(productA.price);
+        // min() method to get min Product price
+        Product productB = productsList.stream()
+                .max((product1, product2)->
+                        product1.price < product2.price ? 1: -1).get();
+        System.out.println(productB.price);
+// Converting product List into Set
+        Set<Float> productPriceList =
+                productsList.stream()
+                        .filter(product->product.price < 30000)   // filter product on the base of price
+                        .map(product->product.price)
+                        .collect(Collectors.toSet());   // collect it as Set(remove duplicate elements)
+        System.out.println(productPriceList);
+
+        Map<Integer,String> productPriceMap =
+                productsList.stream()
+                        .collect(Collectors.toMap(p->p.id, p->p.price+""));
+
+        System.out.println(productPriceMap);
+        // count number of products based on the filter
+        long count = productsList.stream()
+                .filter(product->product.price<30000)
+                .count();
+        System.out.println(count);
+        productsList.stream().filter(p->p.price>25000).forEach(product -> System.out.println ("name::"+product.name));
+
+        List<Float> productPriceList2 =productsList.stream()
+                .filter(p -> p.price > 30000)// filtering data
+                .map(p->p.price)        // fetching price
+                .collect(Collectors.toList()); // collecting as list
+        System.out.println(productPriceList2);
+
+        Stream.iterate(1, element->element+1)
+                .filter(element->element%5==0)
+                .limit(5)
+                .forEach(System.out::println);
+
+    }
+}
